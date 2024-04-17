c52701d69cfa52c3ec8d861c81c450242133e4a2
Suguru Takahashi <60458956+suguruTakahashi-1234@users.noreply.github.com>
2024-04-17 10:38:15 +0900

    add project

---
+++ Development/Development/DevelopmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopmentApp.swift
+//  Development
+//
+//  Created by Suguru Takahashi on 2024/04/17.
+//
+
+import SwiftUI
+
+@main
+struct DevelopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
