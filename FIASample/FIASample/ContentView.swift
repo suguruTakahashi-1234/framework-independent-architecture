9027ed69387bf2ada8c69b1822e685ddd6a3e1e9
Suguru Takahashi <60458956+suguruTakahashi-1234@users.noreply.github.com>
2024-04-25 15:54:36 +0900

    add project

---
+++ FIASample/FIASample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FIASample
+//
+//  Created by Suguru Takahashi on 2024/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FIASample/FIASample/FIASampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIASampleApp.swift
+//  FIASample
+//
+//  Created by Suguru Takahashi on 2024/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FIASampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
