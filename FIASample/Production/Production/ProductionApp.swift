01eabca2adc5b9e3d7ee7a7c3042cfc515e3acc1
Suguru Takahashi <60458956+suguruTakahashi-1234@users.noreply.github.com>
2024-04-26 13:08:42 +0900

    add workspace

---
+++ FIASample/Production/Production/ProductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductionApp.swift
+//  Production
+//
+//  Created by Suguru Takahashi on 2024/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ProductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
